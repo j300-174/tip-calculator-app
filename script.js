06d66e88c2c19e569c42462258a3670e40132376
j300-174 <jeddpicones@gmail.com>
2021-10-13 21:00:13 +0000

    retrieved onclick tip data from html to javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+  get input from Bill
+  get tip
+  get no. of people
+*/
+
+tip1.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip1.value}`);
+});
+
+tip2.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip2.value}`);
+});
+
+tip3.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip3.value}`);
+});
+
+tip4.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip4.value}`);
+});
+
+tip5.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip5.value}`);
+});
+
+// custom
+tip6.addEventListener('click', function(e) {
+  console.log(`testing button clicked, value: ${tip6.value}`);
+});
