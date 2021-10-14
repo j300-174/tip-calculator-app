a73f08fea1b5e3ec5df1ad5770500881157d02e4
j300-174 <jeddpicones@gmail.com>
2021-10-14 17:17:25 +0000

    added space to 5%, fixed reset button width, changed tip to type radio element input

--- script.js
+++ script.js
@@ -5,44 +5,50 @@
 */
 
 const bill = document.getElementById('bill').value;
-console.log(bill);
+// console.log(bill);
 
 const ppl = document.getElementById('people').value;
-console.log(ppl);
+// console.log(ppl);
 
 let tip = 0;
 
-tip1.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip1.value}`);
-  tip = tip1.value;
-  console.log(`tip value: ${tip1.value}`);
-});
-
-tip2.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip2.value}`);
-  tip = tip2.value;
-  console.log(`tip value: ${tip2.value}`);
-});
-
-tip3.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip3.value}`);
-  tip = tip3.value;
-  console.log(`tip value: ${tip3.value}`);
-});
-
-tip4.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip4.value}`);
-  tip = tip4.value;
-  console.log(`tip value: ${tip4.value}`);
-});
-
-tip5.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip5.value}`);
-  tip = tip5.value;
-  console.log(`tip value: ${tip5.value}`);
-});
+// tip1.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip1.value}`);
+//   tip = tip1.value;
+//   console.log(`tip value: ${tip1.value}`);
+// });
+//
+// tip2.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip2.value}`);
+//   tip = tip2.value;
+//   console.log(`tip value: ${tip2.value}`);
+// });
+//
+// tip3.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip3.value}`);
+//   tip = tip3.value;
+//   console.log(`tip value: ${tip3.value}`);
+// });
+//
+// tip4.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip4.value}`);
+//   tip = tip4.value;
+//   console.log(`tip value: ${tip4.value}`);
+// });
+//
+// tip5.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip5.value}`);
+//   tip = tip5.value;
+//   console.log(`tip value: ${tip5.value}`);
+// });
 
 // custom
-tip6.addEventListener('click', function(e) {
-  console.log(`testing button clicked, value: ${tip6.value}`);
-});
+// tip6.addEventListener('click', function(e) {
+//   console.log(`testing button clicked, value: ${tip6.value}`);
+// });
+
+// SETUP TIP AMOUNT
+
+// SETUP TOTAL
+
+// onclick RESET... clear all inputs and reset variables
