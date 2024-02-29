3e9846b2d0a12ea01639b5c2f6a3baf9e1b7b239
Jedd Picones <jeddpicones@gmail.com>
2024-02-29 12:52:19 +0000

    Tip Class Refactor
    - removed comments
    - created tip class
    - separated colour properties
    - sorted event handlers
    - separated functionality into methods

--- script.js
+++ script.js
@@ -1,74 +1,104 @@
-/*
-  get input from Bill
-  get tip
-  get no. of people
-*/
+class Tips {
+  colours = {
+    darkBlue: "#00494d",
+    lightBlue: "#26c2ad",
+    white: "#FFFFFF",
+  };
 
-bill.addEventListener("keyup", calculate);
-custom.addEventListener("keyup", calculate);
-people.addEventListener("keyup", calculate);
+  constructor(tips, labels) {
+    this.tip = tips;
+    this.labels = labels;
+  }
 
-// calculate onclick on tip
-const tip = document.querySelectorAll("input[name='tip\']");
+  activateEventListeners() {
+    bill.addEventListener("keyup", this.calculateTipAmountAndTotal);
+    custom.addEventListener("keyup", this.calculateTipAmountAndTotal);
+    people.addEventListener("keyup", this.calculateTipAmountAndTotal);
+    reset.addEventListener("click", this.resetTipInput);
+  }
 
-for (let i = 0; i < tip.length; i++) {
-  const tipsLabel = tip[i].closest("label");
-  tip[i].onclick = () => {
+  activateTipCalculator() {
+    this.activateEventListeners();
+
+    for (let i = 0; i < this.tip.length; i++) {
+      const tipsLabel = this.tip[i].closest("label");
+      this.tip[i].onclick = () => {
+        this.handleBtnColours(tipsLabel);
+        this.calculateTipAmountAndTotal();
+      };
+    }
+
+    // when custom tip clicked, reset tips to default color
+    // const labels = ;
+    custom.onclick = () => {
+      for (let i = 0; i < this.labels.length; i++) {
+        this.labels[i].style.color = this.colours.white;
+        this.labels[i].style.backgroundColor = this.colours.darkBlue;
+      }
+    };
+
+  }
+
+  resetTipInput() {
+    bill.value = null;
+    people.value = null;
+    custom.value = null;
+    tipAmount.innerHTML = "£0.00";
+    totalAmount.innerHTML = "£0.00";
+    reset.style.color = "var(--dark-green";
+    reset.style.backgroundColor = "#0d686d";
+  }
+
+
+  calculateTipAmountAndTotal() {
+    function activateResetButtonStyling() {
+      reset.style.color = "var(--dark-green)";
+      reset.style.backgroundColor = "var(--light-green)";
+    }
+    activateResetButtonStyling();
+
+    let userTip = 0;
+
+    if (custom.value) {
+      userTip = custom.value / 100;
+    } else if (document.querySelector("input[name='tip']:checked")) {
+      userTip = document.querySelector("input[name='tip']:checked").value / 100;
+    }
+
+    const tip = bill.value * userTip;
+    const total = parseFloat(bill.value) + tip;
+
+    tipAmount.innerHTML = `£${((tip / people.value) * 100) / 100}`;
+    totalAmount.innerHTML = `£${((total / people.value) * 100) / 100}`;
+  }
+
+  // COLOURS
+  handleBtnColours(tipsLabel) {
+    this.setToDefaultColour();
+    this.setClickedColour(tipsLabel);
+  }
+
+  setToDefaultColour() {
     /*
       resets button to default color (only selected receive color change)
       buttons not selected are painted to their default colors
     */
-    for (let i = 0; i < labels.length; i++) {
-      labels[i].style.color = "white";
-      labels[i].style.backgroundColor = "#00494d";
+    for (let i = 0; i < this.labels.length; i++) {
+      this.labels[i].style.color = this.colours.white;
+      this.labels[i].style.backgroundColor = this.colours.darkBlue;
     }
+  }
+
+  setClickedColour(tipsLabel) {
     /*
       after all colors are painted default, clicked button is then changed to
       different color..
     */
-    tipsLabel.style.color = "white";
-    tipsLabel.style.background = "#26c2ad";
+    tipsLabel.style.color = this.colours.white;
+    tipsLabel.style.background = this.colours.lightBlue;
     custom.value = null;
-    calculate();
-  };
+  }
 }
 
-// when custom tip clicked, reset tips to default color
-const labels = document.querySelectorAll(".button");
-custom.onclick = () => {
-  for (let i = 0; i < labels.length; i++) {
-    labels[i].style.color = "white";
-    labels[i].style.backgroundColor = "#00494d";
-  }
-};
-
-// calculate tip amount & total amount
-function calculate() {
-  // activate reset button during any user input
-  reset.style.color = "var(--dark-green)";
-  reset.style.backgroundColor = "var(--light-green)";
-
-  let userTip = 0;
-
-  if (custom.value) {
-    userTip = custom.value / 100;
-  } else if (document.querySelector("input[name='tip']:checked")) {
-    userTip = document.querySelector("input[name='tip']:checked").value / 100;
-  }
-
-  const tip = bill.value * userTip;
-  const total = parseFloat(bill.value) + tip;
-
-  tipAmount.innerHTML = `$${((tip / people.value) * 100) / 100}`;
-  totalAmount.innerHTML = `$${((total / people.value) * 100) / 100}`;
-}
-
-reset.addEventListener("click", () => {
-  bill.value = null;
-  people.value = null;
-  custom.value = null;
-  tipAmount.innerHTML = "$0.00";
-  totalAmount.innerHTML = "$0.00";
-  reset.style.color = "var(--dark-green";
-  reset.style.backgroundColor = "#0d686d";
-});
+var tips = new Tips(document.querySelectorAll("input[name='tip']"), document.querySelectorAll(".button"));
+tips.activateTipCalculator();
